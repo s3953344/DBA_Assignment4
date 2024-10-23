df71e12ab9731f3320733fbc80d86ad98b11885f
s3953344 <76480675+ReusableChopsticks@users.noreply.github.com>
2024-10-23 22:27:30 +1100

    react-hook-form is working!

--- src/pages/ListingPage.tsx
+++ src/pages/ListingPage.tsx
@@ -1,5 +1,6 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
+import { SubmitHandler, useForm } from "react-hook-form";
 import { useNavigate, useParams } from "react-router-dom";
 
 const API_HOST = "http://localhost:3000";
@@ -14,17 +15,28 @@ interface Listing {
   propertyType: string;
 }
 
+type Inputs = {
+  checkIn: Date,
+}
+
+const requiredErrMsg = "This field is required";
+
 export default function ListingPage() {
   const navigate = useNavigate();
   const { id } = useParams();
   const [listing, setListing] = useState<Listing>();
+  const { register, handleSubmit, formState: {errors} } = useForm();
+
+  const onSubmit: SubmitHandler<Inputs> = data => console.log(data);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
         const data = await axios.get(API_HOST + "/api/data/" + id!.toString());
         console.log(data);
-        if (data.data === null) { navigate("/error") }
+        if (data.data === null) {
+          navigate("/error");
+        }
         setListing(data.data);
       } catch (err) {
         console.log(err);
@@ -38,6 +50,22 @@ export default function ListingPage() {
     <div className="container">
       <h1>{listing?.name}</h1>
       <p>{listing?.summary}</p>
+
+      <form
+        onSubmit={handleSubmit((data) => {
+          console.log(data);
+        })}
+      >
+        <label>
+          Check in
+          <input type="date" {...register("checkIn", {required: requiredErrMsg, valueAsDate: true})} />
+          <span>{errors.checkIn?.message?.toString()}</span>
+        </label>
+
+        <button type="submit">
+          Submit
+        </button>
+      </form>
     </div>
   );
 }
